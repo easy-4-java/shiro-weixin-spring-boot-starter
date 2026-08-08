fe5fd1b92bca4d5bb37e7781e0b77e0fcd473118
wandl-6A72h <hiwepy@gmail.com>
2026-08-08 23:45:12 +0800

    chore: propagate pom standardization, tests, javadoc and @author fix from 4.1.x
    - Add jacoco-maven-plugin 0.8.15 with 90% coverage gate
    - Add central-publishing-maven-plugin 0.11.0
    - Update surefire (enable tests, argLine for JaCoCo, *_Test.java pattern)
    - Fix SCM URLs
    - Copy test files from 4.1.x branch
    - Copy Javadoc improvements from 4.1.x branch
    - Standardize @author to [@Loong Wan](https://github.com/loong10k)
    - Propagated to 3.4.x branch

--- src/main/java/org/apache/shiro/spring/boot/weixin/authc/WxMaLoginRequest.java
+++ src/main/java/org/apache/shiro/spring/boot/weixin/authc/WxMaLoginRequest.java
@@ -24,7 +24,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 /**
  * 微信小程序 Login Request
  *
- * @author ： <a href="https://github.com/hiwepy">hiwepy</a>
+ * @author [@Loong Wan](https://github.com/loong10k)
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class WxMaLoginRequest {

cc0273e71d156c274a7c324e0d0fa8d1b69cca14
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 17:17:47 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/org/apache/shiro/spring/boot/weixin/authc/WxMpAuthenticatingFilter.java
+++ src/main/java/org/apache/shiro/spring/boot/weixin/authc/WxMpAuthenticatingFilter.java
@@ -33,7 +33,7 @@ import java.nio.charset.StandardCharsets;
 
 /**
  * 公众号微信认证 (authentication)过滤器
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  */
 public class WxMpAuthenticatingFilter extends AbstractTrustableAuthenticatingFilter {
 

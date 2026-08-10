60e600043fe6128844ea3c6564b5294086568c83
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 17:17:37 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/org/apache/shiro/spring/boot/weixin/token/WxMpAuthenticationToken.java
+++ src/main/java/org/apache/shiro/spring/boot/weixin/token/WxMpAuthenticationToken.java
@@ -21,7 +21,7 @@ import org.apache.shiro.spring.boot.weixin.authc.WxMpLoginRequest;
 /**
  * 微信公众号 Login Token
  *
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  */
 @SuppressWarnings("serial")
 public class WxMpAuthenticationToken extends DefaultAuthenticationToken {

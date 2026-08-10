682546756a7d8e7e958814fab7d67613bf6f423f
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 17:17:31 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/org/apache/shiro/spring/boot/weixin/authc/WxMaAuthenticatingFilter.java
+++ src/main/java/org/apache/shiro/spring/boot/weixin/authc/WxMaAuthenticatingFilter.java
@@ -34,7 +34,7 @@ import java.nio.charset.StandardCharsets;
 
 /**
  * 小程序微信认证 (authentication)过滤器
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  */
 @Slf4j
 public class WxMaAuthenticatingFilter extends AbstractTrustableAuthenticatingFilter {
